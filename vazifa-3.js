c5c795030c634377c33a8f590ff9d8929e625b54
davronmamaraimov1992@gmail.com <davronmamaraimov1996@gmail.com>
2023-05-03 11:04:11 +0500

    Vazifa-3 done

--- vazifa-3.js
+++ vazifa-3.js
@@ -1,6 +1,30 @@
-let x = 11;
-let y = 3;
-let z = 5;
+let x = -1315;
+let y = 0;
+let z = 51123;
 
 
-if(x < y && x < z) && (y < x && y < z);
+if((x > y && x > z) && (y > z)){
+    console.log("O`sish tartibida = " + z + ', ' + y + ', ' + x);
+}
+
+else if ((x > y && x > z) && (z > y)){
+    console.log("O`sish tartibida = " + y + ', ' + z + ', ' + x);
+}
+
+else if ((y > x && y > z) && (z > x)){
+    console.log("O`sish tartibida = " + x + ', ' + z + ', ' + y);
+}
+
+else if ((y > x && y > z) && (x > z)){
+    console.log("O`sish tartibida = " + z + ', ' + x + ', ' + y);
+}
+
+else if ((z > x && z > y) && (x > y)){
+    console.log("O`sish tartibida = " + y + ', ' + x + ', ' + z);
+}
+
+else if ((z > x && z > y) && (y > x)){
+    console.log("O`sish tartibida = " + x + ', ' + y + ', ' + z);
+}
+
+
